58d5cdeb674be999c862b8b55e368156e7b708aa
Ayush Verma <ayushvermacseds@gmail.com>
2025-09-15 14:58:37 +0530

    tuteDute Assignment
    completed

---
+++ basics/assignment/program1.py
@@ -0,0 +1,20 @@
+# Problem Statement: Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+# 3.  Displays the results of each operation on the screen.
+
+a=int(input("Enter a frist Number : "))
+b=int(input("Enter a second Number : "))
+
+# for add
+print("Addition : ",a+b)
+# for subtraction
+print("Subtraction : ",a-b)
+# for Multip
+print("Multiplication : ",a*b)
+# for div
+print("Division : ",a/b)
