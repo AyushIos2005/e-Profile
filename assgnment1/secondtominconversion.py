8605e28c5cb031663165daa8e8efbe84975d48b6
Ayush Verma <ayushvermacseds@gmail.com>
2025-11-13 17:05:54 +0530

    Delete assgnment1 directory

--- assgnment1/secondtominconversion.py
+++
@@ -1,9 +0,0 @@
-
-num = int(input('Enter Second : '))
-hour=int((num/3600))
-print('Hours : ',hour)
-min =int((num%3600)/60)
-print('Minutes : ',min)
-sec=(num%60)
-print('Second : ',sec)
-print(f"{hour} : {min} : {sec}")
