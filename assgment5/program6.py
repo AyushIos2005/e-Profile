6c904b8c5b5cbe723532b66643b3bfc4035baa7d
Ayush Verma <ayushvermacseds@gmail.com>
2025-11-13 17:06:33 +0530

    Delete assgment5 directory

--- assgment5/program6.py
+++
@@ -1,8 +0,0 @@
-# F
-months = ["January","February","March","April","May","June",
-          "July","August","September","October","November","December"]
-
-date = input("Enter date (mm/dd/yyyy): ")
-mm, dd, yy = date.split("/")
-mm = int(mm)
-print(f"{months[mm-1]} {int(dd)}, {yy}")
--- assgment5/program7.py
+++
@@ -1,8 +0,0 @@
-# G
-n = int(input("Enter number of elements in list: "))
-lst = []
-for i in range(n):
-    lst.append(input(f"Enter element {i+1}: "))
-
-rotated = [lst[-1]] + lst[:-1]
-print("After Rotation:", rotated)
