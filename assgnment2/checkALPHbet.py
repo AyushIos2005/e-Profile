9242410136fd4d1e3db6b2205f4e210827ed1d10
Ayush Verma <ayushvermacseds@gmail.com>
2025-11-13 17:06:12 +0530

    Delete assgnment2 directory

--- assgnment2/checkALPHbet.py
+++
@@ -1,7 +0,0 @@
-a = input('Enter any aplhabet : ') [0]
-if(a>='A' and a<='Z'):
-    print("Uppercase")
-elif(a>='a' and a<='z'):
-    print("Lowercase")    
-else:
-    print("INVALID!!")    
--- assgnment2/leapYear.py
+++
@@ -1,5 +0,0 @@
-Y=int(input('Enter a year : '))
-if Y%4==0 and (Y%100!=0 or Y%400==0):
-    print(Y,"is a leap year")
-else:
-    print(Y,"is not leap year")    
